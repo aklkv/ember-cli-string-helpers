89244412f5d9d84618727aa692047818343576d0
Alexey Kulakov <uni_que@me.com>
2025-07-22 07:01:16 -0700

    Update TS types, add warning and examples to README (#379)
    * fix: return types
    
    * fix: update README

--- src/-private/create-string-helper.ts
+++ src/-private/create-string-helper.ts
@@ -2,9 +2,9 @@ import { isHTMLSafe } from '@ember/template';
 
 import type { SafeString } from '@ember/template';
 
-export default function (
-  stringFunction: (input: string) => string | SafeString,
-): (args: [string | SafeString | undefined | null]) => string | SafeString {
+export default function <T extends string | SafeString>(
+  stringFunction: (input: string) => T,
+): (args: [string | SafeString | undefined | null]) => T {
   return function ([input]) {
     if (isHTMLSafe(input)) {
       input = input.toString();
